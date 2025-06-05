df37ae1d5257952cfecda0514fb6477a4ed7e158
svariant <samuele.varianti@nttdata.com>
2025-06-05 16:39:20 +0200

    feat: Test script to generate openapi

---
+++ test/test_generate_openapi.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+## Add the directory containing your package to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+import unittest
+from src.app import app 
+
+class GenerateOpenapi(unittest.TestCase):
+    def test_generate_openapi(self):
+        openapi_dict = app.api_doc
+        import json
+        with open("openapi/openapi.json", "w") as f:
+            json.dump(openapi_dict, f, indent=2)
+        print("openapi.json generated successfully.")
+
+if __name__ == '__main__':
+    unittest.main()
