a918de426b8795267b9342037e33316dee0342e0
svariant <samuele.varianti@nttdata.com>
2025-06-06 16:41:18 +0200

    chore: Fix code review

--- test/test_generate_openapi.py
+++ test/test_generate_openapi.py
@@ -11,7 +11,7 @@ class GenerateOpenapi(unittest.TestCase):
     def test_generate_openapi(self):
         openapi_dict = app.api_doc
         import json
-        with open("../infra/api/v1/openapi.json", "w") as f:
+        with open("infra/api/v1/openapi.json", "w") as f:
             json.dump(openapi_dict, f, indent=2)
         print("openapi.json generated successfully.")
 
