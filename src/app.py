d36dbdc1ac0eba88cfb084d6af0381f489fba9d0
giomella <giomella@emeal.nttdata.com>
2025-07-15 14:36:49 +0200

    [PAGOPA-3137] moved logging configuration in logging_setup.py to configure logging on gunicorn startup

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import os
-import logging
 import traceback
 import json
 import time
@@ -11,91 +10,11 @@ from pydantic import BaseModel, Field, ValidationError
 from flask.wrappers import Response as FlaskResponse
 from configparser import ConfigParser
 from src.anonymizer_logic import anonymize_text_with_presidio
-from logging.config import dictConfig
 from functools import wraps
-from pythonjsonlogger.json import JsonFormatter
 
 ERROR_MESSAGE = "error.message"
 ERROR_TYPE = "error.type"
 ERROR_STACK_TRACE = "error.stack_trace"
-
-
-# Define logger filter
-class ECSContextFilter(logging.Filter):
-    def __init__(self):
-        super().__init__()
-        try:
-            config = ConfigParser()
-            config.read('setup.cfg')
-            self.ecs_fields = {
-                "service.name": config.get("metadata", "name"),
-                "service.version": config.get("metadata", "version"),
-                "service.environment": os.getenv("ENV", "not specified"),
-            }
-        except Exception as e:
-            app.logger.exception("Error building logger properties", extra={
-                ERROR_MESSAGE: str(e),
-                ERROR_TYPE: type(e).__name__,
-                ERROR_STACK_TRACE: traceback.format_exc()
-            })
-
-    def filter(self, record):
-        for key, value in self.ecs_fields.items():
-            setattr(record, key, value)
-        return True
-
-
-# Configure logging
-log_level_str = os.getenv("APP_LOGGING_LEVEL", "INFO").upper()
-log_level = getattr(logging, log_level_str, logging.INFO)
-
-dictConfig({
-    'version': 1,
-    'formatters': {
-        'json': {
-            '()': JsonFormatter,
-            'format': '%(asctime)s %(levelname)s %(name)s %(message)s '
-                      '%(service.name)s %(service.version)s %(service.environment)s '
-                      '%(error.type)s %(error.message)s %(error.stack_trace)s '
-                      '%(method)s %(startTime)s %(requestId)s %(operationId)s %(args)s '
-                      '%(responseTime)s %(status)s %(httpCode)s %(response)s',
-            'rename_fields': {
-                "asctime": "@timestamp",
-                "levelname": "log.level",
-                "name": "log.logger",
-            }
-        },
-    },
-    'filters': {
-        'ecs_context_filter': {
-            '()': ECSContextFilter,
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-            'formatter': 'json',
-            'filters': ['ecs_context_filter'],
-        },
-    },
-    'loggers': {
-        'gunicorn': {
-            'handlers': ['console'],
-            'level': log_level_str,
-            'propagate': False,
-        },
-        'flask.app': {
-            'handlers': ['console'],
-            'level': log_level_str,
-            'propagate': False,
-        }
-    },
-    'root': {
-        'level': log_level_str,
-        'handlers': ['console']
-    }
-})
 
 
 class AnonymizeRequest(BaseModel):
