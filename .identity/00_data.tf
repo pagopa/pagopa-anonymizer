bcec4d733eaec8e03ec0d91025ac83b1b5fb03ce
Francesco <cesareo.francesco@gmail.com>
2025-09-19 12:27:27 +0200

    identity and pipelines

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -13,11 +13,11 @@ data "azurerm_key_vault" "key_vault" {
   resource_group_name = "pagopa-${var.env_short}-sec-rg"
 }
 
-# data "azurerm_user_assigned_identity" "workload_identity_clientid" {
-#   name                = "shared-workload-identity"
-#   resource_group_name = "pagopa-${var.env_short}-${local.location_short}-${var.env}-aks-rg"
-# }
-#
+data "azurerm_user_assigned_identity" "workload_identity_clientid" {
+  name                = "shared-workload-identity"
+  resource_group_name = "pagopa-${var.env_short}-${local.location_short}-${var.env}-aks-rg"
+}
+
 data "azurerm_user_assigned_identity" "identity_cd_01" {
    resource_group_name = "${local.product}-identity-rg"
    name                = "${local.product}-${local.domain}-job-01-github-cd-identity"
@@ -34,8 +34,8 @@ data "azurerm_key_vault_secret" "key_vault_sonar" {
 }
 
 data "azurerm_key_vault_secret" "key_vault_bot_token" {
-  name         = "bot-token-github"
-  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = "pagopa-platform-domain-github-bot-cd-pat"
+  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
 }
 
 data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
@@ -44,6 +44,11 @@ data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
 }
 data "azurerm_key_vault_secret" "key_vault_slack_webhook_deploy" {
   name         = "pagopa-pagamenti-deploy-slack-webhook"
+  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
+}
+
+data "azurerm_key_vault_secret" "key_vault_slack_webhook_integration_test" {
+  name         = "pagopa-pagamenti-integration-test-slack-webhook"
   key_vault_id = data.azurerm_key_vault.domain_key_vault.id
 }
 
